8f246494da94c9362e0cf52958e582f9abf8a47c
osexpert <33638596+osexpert@users.noreply.github.com>
2023-07-04 12:05:28 +0200

    fix

--- nuget push.bat
+++ nuget push.bat
@@ -1,9 +1,9 @@
-set myKey=
+REM set myKey=
 set ver=0.0.1
 
-nuget push bin\Packages\Release\NuGet\GoreRemoting.%ver%.nupkg -s https://api.nuget.org/v3/index.json -k %myKey%
-nuget push bin\Packages\Release\NuGet\GoreRemoting.Compression.Lz4.%ver%.nupkg -s https://api.nuget.org/v3/index.json -k %myKey%
-nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.BinaryFormatter.%ver%.nupkg -s https://api.nuget.org/v3/index.json -k %myKey%
-nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.Json.%ver%.nupkg -s https://api.nuget.org/v3/index.json -k %myKey%
-nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.MemoryPack.%ver%.nupkg -s https://api.nuget.org/v3/index.json -k %myKey%
-nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.MessagePack.%ver%.nupkg -s https://api.nuget.org/v3/index.json -k %myKey%
+nuget push bin\Packages\Release\NuGet\GoreRemoting.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
+nuget push bin\Packages\Release\NuGet\GoreRemoting.Compression.Lz4.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
+nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.BinaryFormatter.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
+nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.Json.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
+nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.MemoryPack.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
+nuget push bin\Packages\Release\NuGet\GoreRemoting.Serialization.MessagePack.%ver%.nupkg -src https://api.nuget.org/v3/index.json -ApiKey %myKey%
